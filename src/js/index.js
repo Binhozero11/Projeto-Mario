c711a322a9c21fabd801f42ac54ffe819d1092cb
Binhozero11 <123664808+Binhozero11@users.noreply.github.com>
2023-01-26 15:24:41 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const modal =  document.querySelector(".modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto")
+
+}
+
+    botaoTrailer.addEventListener("click", () => {
+        alternarModal();
+        video.setAttribute("src",linkDoVideo);
+});
+
+    botaoFecharModal.addEventListener("click", () => {
+         alternarModal()
+         video.setAttribute("src", "");    
+})
